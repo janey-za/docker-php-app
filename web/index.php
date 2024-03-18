57427617c8c2b729905333b3a37c603f685cbff8
Jane Hollander <hollander.jane@gmail.com>
2024-03-18 21:07:44 +0200

    Change web root and add mysql connection.

--- web/index.php
+++ web/index.php
@@ -2,6 +2,22 @@
 
 /**
  * @file
+ * Create a connection using env variables.
  */
 
-phpinfo();
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+try {
+  // Create the connection.
+  $conn = new mysqli('mysql', getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'), getenv('MYdSQL_DATABASE'));
+  if (!$conn->connect_error) {
+    echo "Database connection established.";
+  }
+
+  // @todo Query database and display results.
+  // Close the connection.
+  $conn->close();
+}
+catch (mysqli_sql_exception $e) {
+  // Catch connection error and log it.
+  die("Database connection failed: " . $e->getMessage());
+}
